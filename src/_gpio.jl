baa0dfc78750846ceff6be5561c7e09b81c122f4
mayushibata <b193533@hiroshima-u.ac.jp>
2023-01-25 23:27:29 +0900

    0x20add

--- src/_gpio.jl
+++ src/_gpio.jl
@@ -19,6 +19,11 @@ struct GPIO
 
     function GPIO(ddr::Ptr{UInt8}, port::Ptr{UInt8}, pin::Ptr{UInt8}, bit::Int)
         b = 0x1 << bit
+        if Int64(ddr) < Int64(0x100)
+            ddr = Ptr{UInt8}(Int64(ddr) + Int64(0x20))
+            port = Ptr{UInt8}(Int64(port) + Int64(0x20))
+            pin = Ptr{UInt8}(Int64(pin) + Int64(0x20))         
+        end
         new(ddr, port, pin, b)
     end
 end
