47851ec55ad9d36080db000a08bd7ed41328234f
shibata <105411616+shiaaaaaaaa@users.noreply.github.com>
2023-02-02 04:12:17 +0900

    pullupFlag

--- src/_functions.jl
+++ src/_functions.jl
@@ -58,10 +58,21 @@ Set a given pinmode
 function pinMode(pin::GPIO, m::PinMode)::Nothing
     d = volatile_load(pin.DDR)
     if m == OUTPUT
+        if PullupFlag == 1
+            PullupFlag = 0
+            s = volatile_load(pin.PORT)
+            volatile_store!(pin.PORT, s & ~pin.bit)
+        end
         volatile_store!(pin.DDR, d | pin.bit)
     elseif m == INPUT
+        if PullupFlag == 1
+            PullupFlag = 0
+            s = volatile_load(pin.PORT)
+            volatile_store!(pin.PORT, s & ~pin.bit)
+        end
         volatile_store!(pin.DDR, d & ~pin.bit)
     elseif m == INPUT_PULLUP
+        PullupFlag = 1
         volatile_store!(pin.DDR, d & ~pin.bit)
         s = volatile_load(pin.PORT)
         volatile_store!(pin.PORT, s | pin.bit)
